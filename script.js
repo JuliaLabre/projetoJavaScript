00403f2f86ccbace79ceaefa000c5e8fedc6b30c
Julia Labre <112015111+JuliaLabre@users.noreply.github.com>
2023-10-24 17:26:03 -0300

    merges iniciados

--- script.js
+++ script.js
@@ -23,4 +23,16 @@ const dizerBoasVindas = (nome) => {
   alert(`Olá, ${nome}! Seja bem vindo(a)!`);
 }
 
+function pagamento() {
+  if (realizarPagamento) {
+      const pagamentoFeito = confirm(clienteAtivo + " ,seu procedimento está marcado às " + horarioEscolhido + "\n" + "Realize o pagamento no valor de " + valor);
+      if (pagamentoFeito) {
+          alert(clienteAtivo + " seu procedimento está confirmado e começará no horário marcado" + "\n" +"Agradecemos a preferência, volte sempre!");
+      } else {
+          alert(clienteAtivo + " ,seu procedimento está marcado às " + horarioEscolhido + "\n" + "Realize o pagamento no valor de " + valor + "\n" + "Começaremos assim que recebermos o pagamento");
+      }
+  }
+}
+
+
 main()
