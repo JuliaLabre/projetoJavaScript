6d4c752274ff9b30d751df51b028ba859645962c
eduardo-assimo <eduardo.assimo@gmail.com>
2023-10-26 20:03:26 -0300

    correcao da passagem do parametro de horario agendado para a funcao de geracao de pagamento e recibo, e inclusao da validacao de cpf

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
+// Variáveis de escopo global
 
+//Chamando todas as funções na ordem de execução
 const main = () => {
   const nome = perguntarNome();
   dizerBoasVindas(nome);
-  escolherProcedimento();
-  agendamento()
-  pagamento()
+  const procedimentosEscolhidos = escolherProcedimento();
+  let horarioAleatorio = agendamento();
+  pagamento(nome, procedimentosEscolhidos, horarioAleatorio);
   finalizacao(nome);
+};
 
-}
+//Sistema de salão de beleza que mostra as opções de procedimentos e horários.
 
-//Sistema de salão de beleza para corte de cabelo
-
-//Função que recebe o nome de quem deseja cortar o cabelo
+//Função que recebe o nome e verifica se tem um nome válido
 const perguntarNome = () => {
   let nome = prompt("Qual é o seu nome?");
 
@@ -19,66 +20,79 @@ const perguntarNome = () => {
     nome = prompt("Qual é o seu nome?");
   }
 
-  return nome
-}
+  return nome;
+};
 
 //Função que diz olá e o nome do cliente
 const dizerBoasVindas = (nome) => {
   alert(`Olá, ${nome}! Seja bem vindo(a)!`);
-}
+};
 
 function escolherProcedimento() {
   const procedimentos = [
-    { id: 1, nome: 'Corte', preco: 30.00 },
-    { id: 2, nome: 'Escova', preco: 50.00 },
-    { id: 3, nome: 'Corte e Escova', preco: 70.00 },
-    { id: 4, nome: 'Tintura', preco: 40.00 }
-
+    { id: 1, nome: "Corte", preco: 30.0 },
+    { id: 2, nome: "Escova", preco: 50.0 },
+    { id: 3, nome: "Corte e Escova", preco: 70.0 },
+    { id: 4, nome: "Tintura", preco: 40.0 },
   ];
 
-  //alert("Clique em 'Ok' para ver os procedimentos disponíveis");
-  procedimentos[0].procedimentos.forEach((procedimento) => {
-    console.log(`ID: ${procedimento.id}, Nome: ${procedimento.nome}, Preço: R$ ${procedimento.preco.toFixed(2)}`);
+  procedimentos.forEach((procedimento) => {
+    console.log(
+      `ID: ${procedimento.id}, Nome: ${
+        procedimento.nome
+      }, Preço: R$ ${procedimento.preco.toFixed(2)}`
+    );
   });
 
-  let escolha = parseInt(prompt("Escolha o procedimento pelo ID: \n1 - Corte de cabelo\n2 - Escova\n3 - Corte e Escova"));
-
-  while (isNaN(escolha) || escolha < 1 || escolha > 3) {
-    alert("Por favor, escolha um procedimento disponível")
-    escolha = parseInt(prompt("Escolha o procedimento pelo ID: \n1 - Corte de cabelo\n2 - Escova\n3 - Corte e Escova"));
+  let escolhas = [];
+  let escolha = parseInt(
+    prompt(
+      "Escolha o procedimento pelo ID: \n1 - Corte de cabelo\n2 - Escova\n3 - Corte e Escova\n4 - Tintura\n (Digite 0 para Sair)"
+    )
+  );
+  if(escolha === 0){
+    alert("Programa encerrada")
+    exit()
   }
 
-  switch (escolha) {
-    case 1:
-      const corte = procedimentos[0].procedimentos[0];
-      const resultadoCorte = `Você escolheu o procedimento:\n${corte.nome}\nPreço: R$ ${corte.preco.toFixed(2)}`;
-      document.write(resultadoCorte);
-      break;
-    case 2:
-      const escova = procedimentos[0].procedimentos[1];
-      const resultadoEscova = `Você escolheu o procedimento:\n${escova.nome}\nPreço: R$ ${escova.preco.toFixed(2)}`;
-      document.write(resultadoEscova);
-      break;
-    case 3:
-      const corteEscova = procedimentos[0].procedimentos[2];
-      const resultadoCorteEscova = `Você escolheu o procedimento:\n${corteEscova.nome}\nPreço: R$ ${corteEscova.preco.toFixed(2)}`;
-      document.write(resultadoCorteEscova);
-      break;
-    default:
-      const mensagemErro = "Procedimento não encontrado. Por favor, escolha um ID válido.";
-      document.write(mensagemErro);
+  while (escolha !== 0) {
+    if (!isNaN(escolha) && escolha >= 1 && escolha <= 4) {
+      escolhas.push(procedimentos.find((p) => p.id === escolha));
+    } else {
+      alert("Por favor, escolha um procedimento disponível");
+    }
+    escolha = parseInt(
+      prompt(
+        "Se desejar escolher outro procedimento, digite o ID , se não digite 0 para prosseguir: \n1 - Corte de cabelo\n2 - Escova\n3 - Corte e Escova\n4 - Tintura"
+      )
+    );
   }
+
+  let procedimentosSelecionado = "Você escolheu os procedimentos:\n";
+
+  escolhas.forEach((procedimento) => {
+    procedimentosSelecionado += `${
+      procedimento.nome
+    }\nPreço: R$ ${procedimento.preco.toFixed(2)}\n`;
+  });
+
+  if (escolhas.length === 0) {
+    procedimentosSelecionado = "Nenhum procedimento escolhido.";
+  }
+
+  document.write(procedimentosSelecionado);
+  return escolhas;
 }
 
-
+// Gera horários aleatórios e pede a confirmação do cliente.
 
 function gerarHorarioAleatorio(intervaloInicio, intervaloFim) {
-
   const fracaoAleatoria = Math.random();
 
   const diferenca = intervaloFim - intervaloInicio;
 
-  const horarioAleatorio = intervaloInicio + Math.floor(fracaoAleatoria * diferenca);
+  const horarioAleatorio =
+    intervaloInicio + Math.floor(fracaoAleatoria * diferenca);
 
   const horarioFormatado = horarioAleatorio.toString() + ":00 horas";
 
@@ -86,79 +100,160 @@ function gerarHorarioAleatorio(intervaloInicio, intervaloFim) {
 }
 
 function segundatentativa() {
-
-  let horarioAleatorio = gerarHorarioAleatorio(14, 18);
+  horarioAleatorio = gerarHorarioAleatorio(14, 18);
   let resposta;
 
   do {
-
-    resposta = prompt("Podemos marcar seu atendimento às " + horarioAleatorio + "?\n\n Por favor, digite a letra 's' se sim ou digite a letra 'n' se não.");
+    resposta = prompt(
+      "Podemos marcar seu atendimento às " +
+        horarioAleatorio +
+        "?\n\n Por favor, digite a letra 's' se sim ou digite a letra 'n' se não."
+    );
     resposta.toLowerCase();
 
     switch (resposta) {
-      case 's':
-        alert("Okay, marcado para o horário das " + horarioAleatorio + ". Até lá!");
+      case "s":
+        alert(
+          "Okay, marcado para o horário das " + horarioAleatorio + ". Até lá!"
+        );
         break;
-      case 'n':
+      case "n":
         alert("Que pena, tente agendar outro dia, estaremos à disposição!");
         break;
       default:
         alert("Opção inválida.");
         break;
     }
-
-  } while (resposta != 's' && resposta != 'n');
-
+  } while (resposta != "s" && resposta != "n");
+  
 }
 
 function agendamento() {
-
-  let horarioAleatorio = gerarHorarioAleatorio(7, 11);
+  const horarioAleatorio = gerarHorarioAleatorio(7, 11);
   let resposta;
 
   do {
-
-    resposta = prompt("Podemos marcar seu atendimento às " + horarioAleatorio + "?\n\n Por favor, digite a letra 's' se sim ou digite a letra 'n' se não.");
+    resposta = prompt(
+      "Podemos marcar seu atendimento às " +
+        horarioAleatorio +
+        "?\n\n Por favor, digite a letra 's' se sim ou digite a letra 'n' se não."
+    );
     resposta.toLowerCase();
 
     switch (resposta) {
-
-      case 's':
-
-        alert("Okay, marcado para o horário das " + horarioAleatorio + ". Até lá!");
+      case "s":
+        alert(
+          "Okay, marcado para o horário das " + horarioAleatorio + ". Até lá!"
+        );
         break;
 
-      case 'n':
-
+      case "n":
         segundatentativa();
         break;
 
       default:
-        alert("Opção inválida, por favor digite a letra 's' se sim ou digite a letra 'n' se não.");
+        alert(
+          "Opção inválida, por favor digite a letra 's' se sim ou digite a letra 'n' se não."
+        );
         break;
     }
-
-  } while (resposta != 's' && resposta != 'n');
-
+    
+  } while (resposta != "s" && resposta != "n");
+    return horarioAleatorio;
+ 
 }
 
-//Comanda de serviço
+function pagamento(nome, procedimentosEscolhidos, horarioAleatorio) {
+  confirm(`${nome}, realize o pagamento para confirmar o agendamento`);
 
-
-function pagamento() {
-  if (realizarPagamento) {
-    const pagamentoFeito = confirm(clienteAtivo + " ,seu procedimento está marcado às " + horarioEscolhido + "\n" + "Realize o pagamento no valor de " + valor);
-    if (pagamentoFeito) {
-      alert(clienteAtivo + " seu procedimento está confirmado e começará no horário marcado" + "\n" + "Agradecemos a preferência, volte sempre!");
-    } else {
-      alert(clienteAtivo + " ,seu procedimento está marcado às " + horarioEscolhido + "\n" + "Realize o pagamento no valor de " + valor + "\n" + "Começaremos assim que recebermos o pagamento");
-    }
+  let statusPagamento = processarPagamento(procedimentosEscolhidos, nome, horarioAleatorio);
+  if (statusPagamento) {
+    alert(
+      `${nome} seu procedimento está confirmado e começará no horário marcado.`
+    );
+  } else {
+    alert(
+      `${nome} por favor, realize o pagamento para confirmar o agendamento`
+    );
   }
 }
 
+const processarPagamento = (procedimentosEscolhidos, nome, horarioAleatorio) => {
+  let total = 0;
+
+  procedimentosEscolhidos.forEach((procedimento) => {
+    total += procedimento.preco;
+  });
+
+  let cpfCliente = "";
+
+  do {
+    cpfCliente = prompt("Digite seu CPF:");
+    cpfCliente = validarCPF(cpfCliente);
+  }while (cpfCliente === "")
+   
+
+  let idMeioDePagamento = 0;
+
+  while (
+    isNaN(idMeioDePagamento) ||
+    idMeioDePagamento < 1 ||
+    idMeioDePagamento > 3
+  ) {
+    alert("Por favor, escolha um meio de pagamento");
+    idMeioDePagamento = prompt(
+      "Escolha um meio de pagamento pelo ID: \n1 - Débito\n2 - Crédito\n3 - Pix"
+    );
+  }
+
+  let meioDePagamento;
+  if (idMeioDePagamento == 1) {
+    meioDePagamento = "Débito";
+  } else if (idMeioDePagamento == 2) {
+    meioDePagamento = "Crédito";
+  } else {
+    meioDePagamento = "Pix";
+  }
+
+  // Exibindo o recibo de pagamento
+  alert(`Recibo de Pagamento:
+  Nome: ${nome}
+  CPF: ${cpfCliente}
+  Procedimentos:
+  ${procedimentosEscolhidos
+    .map(
+      (procedimento) =>
+        `${procedimento.nome}: R$ ${procedimento.preco.toFixed(2)}`
+    )
+    .join("\n")}
+  Total: R$ ${total.toFixed(2)}
+  Meio de Pagamento: ${meioDePagamento}
+  Horário Marcado: ${horarioAleatorio}
+  Pagamento realizado com sucesso.`);
+
+  return true;
+};
+
 const finalizacao = (nome) => {
-  alert(`${nome}, Obrigado por nos visitar. Cuide bem desse novo visual e volte sempre quando precisar!`)
+  alert(
+    `${nome}, Obrigado por nos visitar. Cuide bem desse novo visual e volte sempre quando precisar!`
+  );
+};
+
+main();
+
+
+
+
+function validarCPF(cpfCliente){
+
+  let cpfFormatado = cpfCliente.replace(/\D/g, '');
+
+  if(cpfFormatado.length === 11) {
+    return cpfFormatado.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g, "\$1.\$2.\$3-\$4");
+  }else{
+    alert("CPF inválido.");
+    return "";
+  }
 }
 
-
-main()
